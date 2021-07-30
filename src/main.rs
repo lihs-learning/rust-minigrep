9db4733cf9a8143a60279d75d2583a2eaef23f58
LiHS <lihsai0@gmail.com>
2021-07-30 15:42:01 +0800

    extract Config

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,29 @@ use std::fs;
 fn main() {
     let args:Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("query: {}, filename: {}", query, filename);
+    println!("query: {}, filename: {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = &args[1];
+        let filename = &args[2];
+
+        Config{
+            query: query.clone(),
+            filename: filename.clone(),
+        }
+    }
 }
