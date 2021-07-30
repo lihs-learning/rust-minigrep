3d06292e299896db93966318d77df41f6e13934a
LiHS <lihsai0@gmail.com>
2021-07-30 22:13:44 +0800

    add search result lines

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,44 @@ impl Config {
 }
 
 // 运行逻辑
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("文件内容为：\n{}", contents);
+    println!("以下行包含要查询的字符串：");
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
 }
